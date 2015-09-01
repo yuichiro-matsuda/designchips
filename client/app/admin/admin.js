563ab34f6145eb367db6c9670dd7251d916e5e0b
yuichiro-matsuda <Yuichiro@Matsuda-no-MacBook-Pro.local>
2015-09-01 21:43:09 +0900

    initial commit , angular-fullstack generated

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('sampleApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('sampleApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
